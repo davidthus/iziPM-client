3d94023f2c693d75d721deac527ddf2bfd4c8236
davidthus <dvdnicorici05@gmail.com>
2023-03-16 23:50:17 +0000

    get data for project page

--- src/features/projects/Project.tsx
+++ src/features/projects/Project.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { selectProjectById } from "./projectApiSlice";
 
 function Project() {
-  return <div>Project</div>;
+  const { projectId } = useParams();
+  const projectObject = projectId
+    ? useSelector(selectProjectById({ projectId }))
+    : null;
+
+  if (!projectId) {
+    return <div>No project id provided...</div>;
+  }
+  if (!projectObject || projectObject?.isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (projectObject?.notFound) {
+    return <div>Project not found.</div>;
+  }
+  const project = projectObject?.data;
+
+  return (
+    <div>
+      <h1>Project Name: {project?.name}</h1>
+      <p>Project Owner username: {project?.owner.username}</p>
+    </div>
+  );
 }
 
 export default Project;
--- src/features/projects/projectApiSlice.ts
+++ src/features/projects/projectApiSlice.ts
@@ -1,4 +1,6 @@
+import { createSelector } from "@reduxjs/toolkit";
 import { apiSlice } from "../../app/api/apiSlice";
+import { RootState } from "../../app/store";
 import { IProject } from "../../types/project";
 
 interface INewProject {
@@ -28,6 +30,47 @@ export const projectApiSlice = apiSlice.injectEndpoints({
     }),
   }),
 });
+
+type selectProjectArg = {
+  projectId: string;
+};
+
+// Define the selector function
+export const selectProjectById =
+  ({ projectId }: selectProjectArg) =>
+  (state: RootState) => {
+    const {
+      data: projects,
+      isLoading,
+      isError,
+    } = useGetUserProjectsQuery("projects"); // use the query hook
+
+    if (isLoading) {
+      // Return a loading state
+      return { isLoading };
+    }
+
+    if (isError) {
+      // Return an error state
+      return { isError };
+    }
+
+    // Find the projects object with the specified ID
+    let projectsData;
+    if (typeof projects !== "undefined") {
+      projectsData = projects?.projects.find(
+        (project) => project._id === projectId
+      );
+    }
+
+    if (!projectsData) {
+      // Return a not found state
+      return { notFound: true };
+    }
+
+    // Return the MyData object
+    return { data: projectsData };
+  };
 
 export const selectUserProjects =
   projectApiSlice.endpoints.getUserProjects.select("projects");
